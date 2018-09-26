7dc44a609ca6f5a5505501539e8839490c4f7323
Vincent Garcia <24626196+Saitama2016@users.noreply.github.com>
2018-09-26 16:03:27 -0700

    Work on Wardrobe Page

--- src/wardrobe/wardrobe.js
+++ src/wardrobe/wardrobe.js
@@ -1,15 +1,34 @@
 import React from 'react';
+import './wardrobe.css';
 
 export function Wardrobe() {
     return (
         <div>
             <h3>Create your Outfit</h3>
             <section>
-                <p>[Placeholder for avatar based on Skintone]</p>
-                <p>[Dropdown list for Jackets or Coats (Suave)]</p>
-                <p>[Dropdown list for Top Clothing (Shirt, Dress, Tank Top,)]</p>
-                <p>[Dropdown list for Bottom Clothing (Pants, Shorts)]</p>
-                <p>[Dropdown list for Shoes]</p>
+                <div className="user">
+                    <div className="person">
+                        <div className="hat"></div>
+                        <div className="head"></div>
+                        <div className="body">
+                            <div className="top"></div>
+                            <div className="bow"></div>
+                            <div className="left-side"></div>
+                            <div className="right-side"></div>
+                            <div className="shirt"></div>
+                        </div>
+                        <div className="legs">
+                            <div className="left-leg"></div>
+                            <div className="right-leg"></div>
+                        </div>
+                        <div className="feet"></div>
+                    </div>
+                </div>
+            </section>
+            <h3>Checkout your previous Outfits</h3>
+            <section>
+                <p>[Placeholder for outfit log]</p>
+                <p>[Option to edit or delete outfit]</p>
             </section>
         </div>
     );
