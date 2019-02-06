2d95277bcc90e4d85069fd9625ba4116086db1e7
Vincent <garciajvincent@gmail.com>
2019-02-05 19:42:32 -0800

    New Smoke Tests

---
+++ src/wardrobe/outfitFormComps/outfitForm.test.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import { shallow } from 'enzyme';
+
+import OutFitForm from './outfitForm';
+
+describe('<OutFitForm />', () => {
+    it('Renders without crashing', () => {
+        shallow(<OutFitForm />)
+    })
+})
