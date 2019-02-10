b840f60f1dfe45890181a0d4f94b41aa65660fcb
Vincent <garciajvincent@gmail.com>
2019-02-09 22:15:32 -0800

    Include Redux Mock Store

--- src/store.js
+++ src/store.js
@@ -3,5 +3,6 @@ import thunk from 'redux-thunk';
 
 import reducer from './reducer';
 
-export default createStore(reducer, 
-    applyMiddleware(thunk));
+const store = createStore(reducer, applyMiddleware(thunk));
+
+export default store;
