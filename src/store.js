1190023fff351495716aacff1d73e74a15a9abb2
Vincent <garciajvincent@gmail.com>
2019-02-27 12:30:23 -0800

    Fix reducers error

--- src/store.js
+++ src/store.js
@@ -1,7 +1,7 @@
 import { createStore, applyMiddleware } from 'redux';
 import thunk from 'redux-thunk';
 
-import reducer from './reducer';
+import reducer from './reducers/reducer';
 
 const store = createStore(reducer, applyMiddleware(thunk));
 
