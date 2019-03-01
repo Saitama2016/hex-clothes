00d1d967d4ed189490567fd07fffaafb0c000bb8
Vincent <garciajvincent@gmail.com>
2019-02-28 19:37:45 -0800

    Implement Redux-Form in Login Form

--- src/session/loginForm.js
+++ src/session/loginForm.js
@@ -1,24 +1,100 @@
 import React from 'react';
+import { reduxForm, Field, focus, SubmissionError } from 'redux-form';
+import { login } from '../actions/auth';
+import Input from '../input';
+import { required, nonEmpty } from '../validators';
 import './loginForm.css';
-import {fetchUserProfile} from '../actions/users';
 
-// Make function to GET User Info on Submit
+export class Login extends React.Component {
+    
+    onSubmit(values) {
+        return this.props
+            .dispatch(login(values.loginUsername, values.loginPassword))
+            .catch(err => {
+                let { code, reason, message } = err;
+                message = 
+                    code === 401
+                        ? 'Incorrect username or password'
+                        : 'Unable to login, please try again';
+                if (reason === 'ValidationError' || code === 401) {
+                    throw new SubmissionError({
+                        _error: message
+                    })
+                }
+                throw new SubmissionError({
+                    _error: 'Error submitting Login Form'
+                })
+            });
+    }
 
-export default function Login() {
-    return (
-        <div>
-            <h3>Welcome Back!</h3>
-            <form>
-                <fieldset>
-                <label>Username: </label>
-                <input className="loginUsername" type="text" required />
-                <label>Password: </label>
-                <input className="loginPassword" type="password" required />
-                <br />
-                <button className="loginButton" type="submit">Submit!</button>
-                </fieldset>
-            </form>
-            <a href="signup" className="switchToSignUp"><p>Sign Up!</p></a>
-        </div>
-    );
+    render() {
+        let successMessage;
+        if (this.props.submitSucceeded) {
+            successMessage = (
+                <li className='formRow message messageSuccess'>
+                    Form successfully submmitted!                
+                </li>
+            );
+        }
+
+        let errorMessage;
+        if (this.props.error) {
+            errorMessage = (
+                <li className='formRow message messageError'>
+                    {this.props.error}
+                </li>
+            );
+        }
+
+        return (
+            <fieldset className='loginFormContainer'>
+                <legend><h3>Login</h3></legend>
+                <form id='login'
+                    onSubmit={this.props.handleSubmit(values =>
+                        this.onSubmit(values)
+                    )}
+                >
+                    <ul className='formWrapper'>
+                        <li className='formRow'>
+                            <Field 
+                                type='text'
+                                name='loginUsername'
+                                component={Input}
+                                label='Username'
+                                validate={[required, nonEmpty]}
+                            />
+                        </li>
+                        <li className='formRow'>
+                            <Field 
+                                type='password'
+                                name='loginPassword'
+                                component={Input}
+                                label='Password'
+                                validate={[required, nonEmpty]}
+                            />
+                        </li>
+                        <li className='formRow hidden' id='loginErrorRow' hidden>
+                            <p id='loginError'></p>
+                        </li>
+                        {successMessage}
+                        {errorMessage}
+                        <li className='formRow buttonRow'>
+                            <button
+                                type='submit'
+                                id='loginButton'
+                                disabled={this.props.pristine || this.props.submitting}
+                            >
+                            Login                                
+                            </button>
+                        </li>
+                    </ul>
+                </form>
+            </fieldset>
+        )
+    }
 }
+
+export default reduxForm({
+    form: 'login',
+    onSubmitFail: (errors, dispatch) => dispatch(focus('login', Object.keys(errors)[0]))
+})(Login);
