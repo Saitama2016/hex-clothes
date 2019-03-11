61c1d3709f4cb12d38bd7f134b25df0957a0f92e
Vincent <garciajvincent@gmail.com>
2019-03-10 17:39:53 -0700

    Fix Side Navigation Bar View and add Animations to Wardrobe page

--- src/wardrobe/wardrobe.js
+++ src/wardrobe/wardrobe.js
@@ -1,21 +1,31 @@
 import React from 'react';
 import { connect } from 'react-redux';
 import './wardrobe.css';
+
 import Model from './model';
 import OutFitForm from './outfitFormComps/outfitForm';
+import TopNavLogOut from '../navigation/topNavLogOut';
+import SideNavLogOut from '../navigation/sideNavLogOut';
 
 const Wardrobe = () => {
     
     return (
         <div>
-            <h3>Create your Outfit</h3>
-            <section>
+            <header className="wardrobeHeader">
+                <i onClick={() => document.getElementById("mySidenav").style.width = "500px"} className="fas fa-bars"></i>
+                <h1 className="App-logo">HexClothes</h1>
+                <h2 className="App-title">Make outfits that complement you!</h2>
+                <TopNavLogOut />
+            </header>
+            <SideNavLogOut />
+            <h3 className="wardrobeInstructions">Create your Outfit</h3>
+            <section className="createOutfitForm">
                 <Model />
                 <br />
                 <OutFitForm />
             </section>
-            <h3>Checkout your previous Outfits</h3>
-            <section>
+            <h3 className="wardrobeInstructions">Checkout your previous Outfits</h3>
+            <section className="outfitLog">
                 <p>[Placeholder for outfit log]</p>
                 <p>[Option to edit or delete outfit]</p>
             </section>
